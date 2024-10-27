ff51d72c572253f96d8f27c1ac52bf065db6058c
stbow <sab805@gmail.com>
2024-10-27 15:07:33 -0700

    finished number sorter lesson

--- 03-phone-num-validator/number-sorter-script.js
+++ 03-phone-num-validator/number-sorter-script.js
@@ -7,7 +7,11 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
 
-  updateUI(inputValues);
+  const sortedValues = inputValues.sort((a, b) => {
+    return a - b;
+  }); //update this function call depending on which kind of sort you want to use, e.g. = selectionSort(inputValues);
+
+  updateUI(sortedValues);
 }
 
 const updateUI = (array = []) => {
@@ -31,5 +35,41 @@ const bubbleSort = (array) => {
   }
   return array;
 };
+
+/* This time, you'll be implementing a selection sort. Selection sort works by finding the smallest value in the array, then swapping it with the first value in the array. Then, it finds the next smallest value in the array, and swaps it with the second value in the array. It continues iterating through the array until it is completely sorted. */
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+  return array;
+
+}
+
+/* The last sorting algorithm you will implement is the insertion sort. This algorithm works by building up a sorted array at the beginning of the list. It begins the sorted array with the first element. Then it inspects the next element and swaps it backward into the sorted array until it is in a sorted position, and so on. */
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
+  }
+  return array;
+}
 
 sortButton.addEventListener("click", sortInputArray);
